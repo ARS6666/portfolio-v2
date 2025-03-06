cb9c0cb148c4607f1c32f6ee3c78c16bc52e3312
ARS6666 <ARSprogramming123@gmail.com>
2025-03-05 22:09:02 -0800

    footer.js finished

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import "../assets/css/footer.css"
-import phone from "../assets/media/phone.png"
+import "../assets/css/footer.css";
+import phone from "../assets/media/phone.png";
+
 function Footer() {
     return (
-        <div className='footer-container'>
+        <div className='footer-container fontr'>
             <div className="footer-title-container">
-                <span className="footer-title fontr">برقراری ارتباط</span>
+                <span className="footer-title">برقراری ارتباط</span>
             </div>
-            <div className='footer-phone-container'>
+            <div className='footer-phone-container' dir="rtl">
                 <img src={phone} className='footer-phone' />
+                <div className='footer-texts-container'>
+                    <div className='footer-phone-text-container'>
+                        <span className='footer-phone-text'>تلفن همراه:</span>
+                        <span className='footer-phone-text'>09915910538</span>
+                    </div>
+                    <div className='footer-email-container'>
+                        <span className='footer-email'>ایمیل:</span>
+                        <span className='footer-email'>arsprogramming123@gmail.com</span>
+                    </div>
+                    <div className='footer-telegram-container'>
+                        <span className='footer-telegram'>آیدی تلگرام:</span>
+                        <span className='footer-telegram'>ARSARS66@</span>
+                    </div>
+                </div>
             </div>
-
-            <span className='footer-phone'></span>
-            <span className='footer-email'></span>
-            <span className='footer-telegram'></span>
         </div>
     );
 }
