8629aaabbdedfbe7fb39a16d254988f5246e7514
ARS6666 <ARSprogramming123@gmail.com>
2025-03-12 18:30:34 +0330

    working on worksamples page

--- src/components/home/projects.js
+++ src/components/home/projects.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import "../../assets/css/home/projects.css"
-import productimg from "../../assets/media/8.png"
+import meszanajn from "../../assets/media/worksamples/mh3.PNG"
+import ida from "../../assets/media/worksamples/ida1.PNG"
 
 function Projects() {
     return (
         <>
             <div class="projects-title-container">
-                <span class="projects-title fontr">کار هایی که تا الان انجام دادم</span>
+                <span class="projects-title fontr">... کار هایی که تا الان انجام دادم</span>
             </div>
             <div className='projects-card-container fontr'>
-                <div className='project-card'>
-                    <img className='project-image' src={productimg}/>
-                    <span className='project-name'>مس هنر زنجان</span>
-                    <span className='project-type'>سایت فروشگاهی</span>
-                </div>
-                <div className='project-card fontr'>
-                    <img className='project-image' src={productimg}/>
-                    <span className='project-name'>IDA-LTD</span>
-                    <span className='project-type'>سایت شرکتی</span>
-                </div>
-            </div>
+                <a className='link-deco-black' href="/worksample?id=1">
+                    <div className='project-card'>
+                        <img className='project-image' src={meszanajn} />
+                        <span className='project-name'>مس هنر زنجان</span>
+                        <span className='project-type'>سایت فروشگاهی</span>
+                    </div> </a>
+                <a className='link-deco-black' href="/worksample?id=1">
+                    <div className='project-card fontr'>
+                        <img className='project-image' src={ida} />
+                        <span className='project-name'>IDA-LTD</span>
+                        <span className='project-type'>سایت شرکتی</span>
+                    </div></a>
+            </div >
 
         </>
     );
